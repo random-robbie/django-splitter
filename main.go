efc3c0938d30075f2f3e46d5eaf165ebb153e86c
Robbie <txt3rob@gmail.com>
2018-10-19 08:54:35 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,130 @@
+/*
+ * Copyright @random_robbie (c) 2018.
+ */
+
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/logrusorgru/aurora"
+	"github.com/remeh/sizedwaitgroup"
+	"io/ioutil"
+)
+
+var (
+	fileofurls  = "list.txt"
+	outputfile  = "./cfg/"
+	filepathurl = "/static../settings.py"
+	au          aurora.Aurora
+	colors      = flag.Bool("colors", true, "enable or disable colors")
+)
+
+func init() {
+	flag.Parse()
+	au = aurora.NewAurora(*colors)
+}
+
+func grabURL(URL string, output string, filepathurl string, swg *sizedwaitgroup.SizedWaitGroup) {
+
+	defer swg.Done()
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{Transport: tr}
+
+	newurl := "https://" + URL + filepathurl
+	resp, err := client.Get(newurl)
+	if err != nil {
+		// handle err
+		log.Printf("could not send request: %v", err)
+	}
+
+	resp.Header.Set("Accept-Encoding", "gzip, deflate")
+	resp.Header.Set("Accept", "*/*")
+	resp.Header.Set("Accept-Language", "en")
+	resp.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0")
+	fmt.Println("[*] Testing ", newurl, "[*]")
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		fmt.Println(au.Red("[*] Page Not Found [*]"))
+	}
+	if resp.StatusCode == 403 {
+		fmt.Println(au.Red("[*] Access Denied [*]"))
+	}
+	if resp.StatusCode == 401 {
+		fmt.Println(au.Red("[*] Access Denied [*]"))
+	}
+	if resp.StatusCode == 301 {
+		fmt.Println(au.Red("[*] Redirected - Not Found[*]"))
+	}
+	if resp.StatusCode == 302 {
+		fmt.Println(au.Red("[*] Redirected - Not Found [*]"))
+	}
+
+	if resp.StatusCode == 500 {
+		fmt.Println(au.Red("[*] Server Error 500 [*]"))
+	}
+
+	if resp.StatusCode == 200 {
+		if resp.Body != nil {
+			bodyBytes, nil := ioutil.ReadAll(resp.Body)
+			bodyString := string(bodyBytes)
+			htmlcontent := strings.Contains(bodyString, "<head>")
+			if htmlcontent == false {
+
+
+				capturedfile := outputfile + URL + ".txt"
+
+				htmlfile, err := os.Create(capturedfile)
+
+				if err != nil {
+					log.Fatalf("could not create file: %v", err)
+					os.Exit(1)
+				}
+
+				defer htmlfile.Close()
+				fmt.Println(au.Green("[*] Saving to file [*]"))
+				io.Copy(htmlfile, resp.Body)
+
+			}
+
+		}
+
+	}
+}
+
+func main() {
+	swg := sizedwaitgroup.New(10)
+	fmt.Println(au.Blue("[*] Nginx Alias Checker - By @random_robbie [*]"))
+	file, err := os.Open(fileofurls)
+	if err != nil {
+		log.Fatalf("unable to open file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		swg.Add()
+		URL := strings.TrimSpace(scanner.Text())
+		go grabURL(URL, outputfile, filepathurl, &swg)
+		swg.Wait()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
